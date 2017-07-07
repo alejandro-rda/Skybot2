67ae8447b83282a5374d72ae44c15a8d1a5f045a
Ing. Ruiz <lrda@outlook.com>
2017-07-07 00:38:08 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: '82ff654b-3087-4b37-beef-2bd10868c0b7',
+    appPassword: '02aYSbsvEiJLTMqmfi2FjpO'
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+
+    if(session.message.text === 'Daniel' || session.message.text === 'Dan')
+    {
+        session.send("Erazo mariconazo");
+    }
+
+});
