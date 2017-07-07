671169e7b0711b084cc94bf3fca484071bd56e1f
Ing. Ruiz <lrda@outlook.com>
2017-07-07 00:49:00 -0500

    Mejoras al codigo

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
 
-    if(session.message.text === 'Daniel' || session.message.text === 'Dan')
+    if(session.message.text === 'Daniel'
+        || session.message.text === 'daniel'
+        || session.message.text === 'Dan'
+        || session.message.text === 'dan')
     {
         session.send("Erazo mariconazo");
     }
