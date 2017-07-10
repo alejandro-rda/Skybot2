81e03901d108b43ca45cc13fb04338e44aa1f358
rualejan <lrda@outlook.com>
2017-07-10 00:54:50 -0500

    Mejoras al bot

--- app.js
+++ app.js
@@ -13,22 +13,64 @@ var connector = new builder.ChatConnector({
     appPassword: '02aYSbsvEiJLTMqmfi2FjpO'
 });
 
-var regex = /.*Dan.*/;
-var regex2 = /.*dan.*/;
+/*
+var regex = /.*Dan.*!/;
+var regex2 = /.*dan.*!/;
+*/
+
+var regex3 = /.*Loco.*/;
+var regex4 = /.*loco.*/;
+var regex5 = /.*Alejandro.*/;
+var regex6 = /.*alejandro.*/;
 
 // Listen for messages from users
-
 server.post('https://skybot-danielazo.herokuapp.com/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+// Receive messages from the user
 var bot = new builder.UniversalBot(connector, function (session) {
 
     var mensaje = session.message.text;
 
     if
-    (mensaje.match(regex)||mensaje.match(regex2))
-    {
-        session.send("Erazo mariconazo");
+     (mensaje.match(regex3)||mensaje.match(regex4)||mensaje.match(regex5)||mensaje.match(regex6)) {
+        session.send("Lo siento no puedo molestar a mi creador.");
+    }else{
+        session.send(mensaje + " mariconazo");
+    }
+});
+
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        var membersAdded = message.membersAdded
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text('Bienvenido ' + membersAdded + ' :)'));
     }
 
+    if (message.membersRemoved && message.membersRemoved.length > 0) {
+        var membersRemoved = message.membersRemoved
+            .map(function (m) {
+                var isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+
+        bot.send(new builder.Message()
+            .address(message.address)
+            .text('El siguiente miembro ' + membersRemoved + ' se ha quitado :('));
+
+        membersRemoved.map(function (m) {
+            if(m.id === 'dan.erazog'){
+                bot.send(new builder.Message()
+                    .address(message.address)
+                    .text('Dan se ha salido, alguien agreguelo porfa :S'));
+            }
+        });
+    }
 });
