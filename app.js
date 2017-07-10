fb6e98a3d9eea313ad8567cd16138f67e1daa919
rualejan <lrda@outlook.com>
2017-07-10 01:22:47 -0500

    Mejoras en los nombres al bot

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ bot.on('conversationUpdate', function (message) {
         var membersAdded = message.membersAdded
             .map(function (m) {
                 var isSelf = m.id === message.address.bot.id;
-                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.name + ')';
             })
             .join(', ');
 
@@ -54,16 +54,19 @@ bot.on('conversationUpdate', function (message) {
     }
 
     if (message.membersRemoved && message.membersRemoved.length > 0) {
+        var memberRemoved = null;
         var membersRemoved = message.membersRemoved
             .map(function (m) {
+                memberRemoved = m.name;
                 var isSelf = m.id === message.address.bot.id;
                 return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
             })
             .join(', ');
 
+
         bot.send(new builder.Message()
             .address(message.address)
-            .text('El siguiente miembro ' + membersRemoved + ' se ha quitado :('));
+            .text('El siguiente miembro ' + memberRemoved + ' se ha quitado :('));
 
         membersRemoved.map(function (m) {
             if(m.id === 'luiso_xd'){
