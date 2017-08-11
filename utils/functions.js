ee14b29257cc2abef4dc9788db3f3991a125eb24
rualejan <lrda@outlook.com>
2017-08-11 18:21:07 -0500

    Gran Mejora

--- utils/functions.js
+++ utils/functions.js
@@ -4,10 +4,13 @@
 
 exports.devolvermensaje = function (mensaje, mensajeM, lstMensajes) {
 
+    if(mensajeM.includes('@')) {
+        mensajeM = mensaje.split('@').reverse().pop();
+    }
+
     for (let mensaje of lstMensajes) {
 
         let regex = new RegExp(mensaje.name, "i");
-
         if (mensajeM.match(regex)) {
             return mensaje.value;
         }
